a7798a820e36e5b86eb64a004cae17df427859d1
Özer Öztürk <60944453+OzerOzturk@users.noreply.github.com>
2021-02-05 23:42:26 +0300

    running_js and conditionals

---
+++ Projects/Conditionals (166-168)/app.js
@@ -0,0 +1,49 @@
+
+
+/* let random = Math.random();
+if (random<0.5) {
+    console.log("your number is less than 0.5!")
+} else {
+    console.log("your number is greater (or equal) than 0.5!")
+}
+console.log(random) */
+
+/* 
+const dayOfWeek = prompt("ENTER A DAY").toLowerCase();
+
+if(dayOfWeek === "monday") {
+    console.log("uppsss")
+}
+else if (dayOfWeek === "saturday") {
+    console.log("I love saturdays!")
+} 
+else if (dayOfWeek === "friday") {
+    console.log("Fridays are really cool")
+} else {
+    console.log("MEH")
+} 
+
+
+
+ /* 0-5 - FREE
+ 5 - 10 CHILD $10
+ 10 - 65 ADULT $20
+ 65+ SENIOR $10 */
+
+
+
+ const age = 89;
+
+ if (age <5 ) {
+     console.log("you are a baby, you get in for free")
+ }  
+//  if 1st case is not correct, then its time to go 2nd case //
+ else if (age <10) {   
+    console.log("you are a child, you pay $10")
+ } else if (age <65) {
+    console.log("you are an adult, you pay $20")
+ } else {
+     console.log("you are a senior, you pay $10")
+ }
+ 
+ 
---
+++ Projects/Runnig_JS (165)/app.js
@@ -0,0 +1,3 @@
+console.log("HELLO FROM OUR FIRST JS FILE");
+let total = 1+3;
+console.log("GOODBYE!")
