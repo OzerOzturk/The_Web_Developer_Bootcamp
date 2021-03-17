f75a8e397b6315dc577df41c20a5fe6d30db4d7d
Özer Öztürk <60944453+OzerOzturk@users.noreply.github.com>
2021-03-18 00:07:32 +0300

    conditions, prompts, items added on todo.js

--- Projects/Todo List Project (202)/todo.js
+++ Projects/Todo List Project (202)/todo.js
@@ -1,1 +1,26 @@
-alert("deneme")
+let input = prompt("what would you like to do?");
+const todos = ["Collect Chicken Eggs", "Clean Litter Box"];
+while (input !=="quit" && input !== "q") {
+    if (input === "list") {
+        console.log("**************")
+        for(let i=0; i < todos.length; i++) {
+            console.log(`${i}: ${todos[i]}`);
+        }
+        console.log("**************")
+    } else if (input === "new"){
+        const newTodo = prompt("ok,what's the new todo?");
+        todos.push(newTodo);
+        console.log(`${newTodo} added to the list!`) 
+    } else if(input === "delete"){
+        const index = parseInt(prompt("ok, enter an index to delete:")); 
+        if(!Number.isNaN(index)){
+            const deleted = todos.splice(index, 1);
+            console.log(`Ok, deleted ${deleted[0]}`);
+        } else {
+            console.log("unknown index")
+        }
+        
+    } 
+    input = prompt("what would you like to do?")
+}
+console.log("OK QUIT THE APP")
